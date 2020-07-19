e17eea19432c8a92f7e831ca5d719e869d21b2c4
Zyrakia <mailzyrakia@gmail.com>
2020-07-19 10:55:47 -0700

    Initial setup

--- CommandExecutor.ts
+++
@@ -1,5 +0,0 @@
-import CommandOrigins from "./CommandOrigins";
-
-export default interface CommandExecutor {
-    invoke(origins: CommandOrigins): void;
-}
--- CommandOrigins.ts
+++
@@ -1,9 +0,0 @@
-import { Userstate, Client } from "tmi.js";
-
-export default interface CommandOrigins {
-    command: string;
-    arguments: string[];
-    channel: string;
-    user: Userstate;
-    client: Client;
-}
