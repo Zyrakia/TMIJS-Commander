8b06a5115ad8df3bad87811978538a0c7a17d3da
Ole <43284152+Zyrakia@users.noreply.github.com>
2020-09-14 13:55:05 -0700

    Update CommandExecutor.ts

--- src/types/CommandExecutor.ts
+++ src/types/CommandExecutor.ts
@@ -1,9 +1,9 @@
 import { CommandOrigins } from '../Exporter';
 
 export abstract class CommandExecutor {
-	abstract invoke(origins: CommandOrigins): void;
+	abstract async invoke(origins: CommandOrigins): Promise<void>;
 }
 
 export interface AnonymousCommandExecutor {
-	(origins: CommandOrigins): void;
+	async (origins: CommandOrigins): Promise<void>;
 }
